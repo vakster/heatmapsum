69d8bd33ce0acf6f8284adbb9c6ec24161c56c2d
Patrick Monaghan <monaghp1@tcnj.edu>
2015-11-15 05:45:32 -0500

    Update featureMap.js

--- featureMap.js
+++ featureMap.js
@@ -29,12 +29,12 @@ function initialize() {
       },
       
       styles: [
-       { where: "col2 > 30000",
+       { where: "col3 > 30000",
          markerOptions: {
             iconName: 'small_blue'
          }
        },
-       { where: "col2 < 30000",
+       { where: "col3 < 30000",
          markerOptions: {
             iconName: 'small_purple'
          }
