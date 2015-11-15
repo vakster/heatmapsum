f0e69762d8b0bf2a24471859c9f16f9d352542f6
JStack424 <JStack424@gmail.com>
2015-11-14 20:12:00 -0500

    added echoes for all data

--- School.php
+++ School.php
@@ -10,10 +10,10 @@
         private $zipCode;
         private $address;
         private $city;
-        private $inStateTuition;
-        private $outStateTuition;
-        private $inStateCoeff;
-        private $outStateCoeff;
+        private $inStateTuition = array();
+        private $outStateTuition = array();
+        private $inStateCoeff == array();
+        private $outStateCoeff == array();
         
         //Functions:
         //Initializer(All data) -> store in respective locations, call function 2
@@ -32,34 +32,44 @@
         
         //Create arrays for in and out of state in appropriate variables
         private function storeTuitionData($whichArray, $data14, $data13, $data12, $data11, $data10, $data9, $data8, $data7, $data6, $data5, $data4, $data3, $data2) {
+            echo "Which array: " . $whichArray . "<br>";
+            
             if ($whichArray == "in") {
                 $this->inStateTuition = array("14" => $data14, "13" => $data13, "12" => $data12, "11" => $data11, "10" => $data10, "9" => $data9, "8" => $data8, "7" => $data7, "6" => $data6, "5" => $data5, "4" => $data4, "3" => $data3, "2" => $data2);
+                echo "In state tuition: " . $inStateTuition . "<br>";
             }
             else if ($whichArray == "out") {
                 $this->outStateTuition = array("14" => $data14, "13" => $data13, "12" => $data12, "11" => $data11, "10" => $data10, "9" => $data9, "8" => $data8, "7" => $data7, "6" => $data6, "5" => $data5, "4" => $data4, "3" => $data3, "2" => $data2);
+                echo "Out of state tuition: " . $outStateTuition . "<br>";
             }
             else
                 echo "Error";
         }
-
+        
         //Calculates the quadratic coefficients for in and out of state tuition and stores them in appropriate arrays
         private function calcCoeffs() {
             $this->quadReg("in");
+            echo "In state coeff: " . $inStateCoeff . "<br>";
+            
             $this->quadReg("out");
-            echo "In state: " . $inStateCoeff . "<br>";
-            echo "Out of state: " . $outStateCoeff . "<br>";
+            echo "Out of state coeff: " . $outStateCoeff . "<br>";
         }
-                
+        
         //Get quadratic formula coefficients() -> calculates values of a, b, and c for quadratic equation
         public function quadReg($whichArray) {
-            
+            echo "Which array: " . $whichArray . "<br>";
             //Count the number of values
-            $tuitionValues;
-            if ($whichArray == "in")
+            $tuitionValues = array();
+            if ($whichArray == "in") {
                 $tuitionValues = $inStateTuition;
-            else if ($whichArray == "out")
+                echo "In tuition values: " . $tuitionValues . "<br>";
+            }
+            else if ($whichArray == "out") {
                 $tuitionValues = $outStateTuition;
+                echo "Out tuition values: " . $tuitionValues . "<br>";
+            }
             $numValues = count($tuitionValues);
+            echo "There are " . $numValues . " total tuition values<br>";
             
             //Find Sum of x, y, x^2,, x^3, x^4, x*y, x^2*y
             //Initialize each sum to 0
@@ -81,6 +91,7 @@
                 $sumxy += $key*$currentYearCost;
                 $sumx2y += $key*$key*$currentYearCost;
             }
+            echo "Sums:<br>" . $sumx . "<br>" . $sumy . "<br>" . $sumx2 . "<br>" . $sumx3 . "<br>" . $sumx4 . "<br>" . $sumxy . "<br>" . $sumx2y . "<br>";
             
             // a = (( Σ x2y * Σ xx ) - (Σ xy * Σ xx2 )) / (( Σ xx * Σ x2x2) - (Σ xx2 * Σ xx2))
             // b = (( Σ xy * Σ x2x2 ) - (Σ x2y * Σ xx2 )) / (( Σ xx * Σ x2x2) - (Σ xx2 * Σ xx2 ))
@@ -95,7 +106,7 @@
             $a = (( (($sumx2y) - (($sumx2 * $sumy) / $numValues)) * (($sumx2) - (($sumx * $sumx) / $numValues)) ) - ((($sumxy) - (($sumx * $sumy) / $numValues)) * (($sumx3) - (($sumx2 * $sumx) / $numValues)) )) / (( (($sumx2) - (($sumx * $sumx) / $numValues)) * (($sumx4) - (($sumx2*$sumx2) / $numValues))) - ((($sumx3) - (($sumx2 * $sumx) / $numValues)) * (($sumx3) - (($sumx2 * $sumx) / $numValues)) ));
             $b = (( (($sumxy) - (($sumx * $sumy) / $numValues)) * (($sumx4) - (($sumx2*$sumx2) / $numValues)) ) - ((($sumx2y) - (($sumx2 * $sumy) / $numValues)) * (($sumx3) - (($sumx2 * $sumx) / $numValues)) )) / (( (($sumx2) - (($sumx * $sumx) / $numValues)) * (($sumx4) - (($sumx2*$sumx2) / $numValues))) - ((($sumx3) - (($sumx2 * $sumx) / $numValues)) * (($sumx3) - (($sumx2 * $sumx) / $numValues)) ));
             $c = ( $sumy / $numValues ) - ( $b * ( $sumx / $numValues )) - ( $a * ( $sumx2 / $numValues ));
-            
+            echo "a: " . $a . " b: " . $b . " c: " . $c "<br>";
             if ($whichArray == "in")
                 $this->inStateCoeff = array($a, $b, $c);
             else if ($whichArray == "out")
@@ -108,6 +119,7 @@
         public function getEstimatedTuition($whichArray, $predictionYear) {
             if (($predictionYear >= 2002) && ($predictionYear <= 2100)) {
                 $predictionYear -= 2000;
+                echo "year: " . $predictionYear . "<br>";
                 $predictedTuition = 0;
                 if ($whichArray == "in")
                     $predictedTuition = ($inStateCoeff[0]*$predictionYear*$predictionYear + $inStateCoeff[1]*$predictionYear + $inStateCoeff[2]);
