18248e59936534076bd04179f9c47e07610b43ce
JStack424 <JStack424@gmail.com>
2015-11-14 19:48:06 -0500

    updated school

--- School.php
+++ School.php
@@ -24,10 +24,10 @@
             $this->zipCode = $zipCode;
             $this->address = $address;
             $this->city = $city;
-            storeTuitionData("in", $in14, $in13, $in12, $in11, $in10, $in9, $in8, $in7, $in6, $in5, $in4, $in3, $in2);
-            storeTuitionData("out", $out14, $out13, $out12, $out11, $out10, $out9, $out8, $out7, $out6, $out5, $out4, $out3, $out2);
-            calcCoeffs(); // ***MOVE THIS FUNCTION CALL OUTSIDE CONSTRUCTOR***
-            echo "Newman is a bitch";
+            $this->storeTuitionData("in", $in14, $in13, $in12, $in11, $in10, $in9, $in8, $in7, $in6, $in5, $in4, $in3, $in2);
+            $this->storeTuitionData("out", $out14, $out13, $out12, $out11, $out10, $out9, $out8, $out7, $out6, $out5, $out4, $out3, $out2);
+            $this->calcCoeffs(); // ***MOVE THIS FUNCTION CALL OUTSIDE CONSTRUCTOR***
+            echo "" . $institution . " object created!<br>";
         }
         
         //Create arrays for in and out of state in appropriate variables
@@ -44,10 +44,10 @@
 
         //Calculates the quadratic coefficients for in and out of state tuition and stores them in appropriate arrays
         private function calcCoeffs() {
-            quadReg("in");
-            echo "Newman is bad ";
-            quadReg("out");
-            echo "at cornhole";
+            $this->quadReg("in");
+            $this->quadReg("out");
+            echo "In state: " . $inStateCoeff . "<br>";
+            echo "Out of state: " . $outStateCoeff . "<br>";
         }
                 
         //Get quadratic formula coefficients() -> calculates values of a, b, and c for quadratic equation
