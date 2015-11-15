6a37c6e666c6a8b5b7ebef80010a962488b38ce7
Patrick Monaghan <monaghp1@tcnj.edu>
2015-11-14 23:26:13 -0500

    Update featureMap.js

--- featureMap.js
+++ featureMap.js
@@ -8,7 +8,7 @@
       viewport.setAttribute('content', 'initial-scale=1.0, user-scalable=no');
     }
     var mapDiv = document.getElementById('googft-mapCanvas');
-    mapDiv.style.width = isMobile ? '100%' : '800px';
+    mapDiv.style.width = isMobile ? '100%' : '100%';
     mapDiv.style.height = isMobile ? '100%' : '600px';
     var map = new google.maps.Map(mapDiv, {
       center: new google.maps.LatLng(46.768862265500054, -112.58358379999999),
