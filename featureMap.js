8a359d4ee0b51ab96d224713e42f8305f4358258
Patrick Monaghan <monaghp1@tcnj.edu>
2015-11-15 01:13:29 -0500

    Update featureMap.js

--- featureMap.js
+++ featureMap.js
@@ -44,5 +44,16 @@ function initialize() {
       }
     }
   }
-
+// Update the query sent to the Fusion Table Layer based on
+      // the user selection in the select menu
+      function updateMap(layer, tableId, locationColumn) {
+        var delivery = document.getElementById('delivery').value;
+        if (delivery) {
+          layer.setOptions({
+            query: {
+              select: locationColumn,
+              from: tableId,
+              where: "delivery = '" + delivery + "'"
+            }
+          });
   google.maps.event.addDomListener(window, 'load', initialize);
