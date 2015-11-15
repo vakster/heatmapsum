42355d6c82be6ddb230edc0067b7fb9e5bc9cd02
JStack424 <JStack424@gmail.com>
2015-11-14 19:30:37 -0500

    updated School class

--- School.php
+++ School.php
@@ -27,7 +27,6 @@
             storeTuitionData("in", $in14, $in13, $in12, $in11, $in10, $in9, $in8, $in7, $in6, $in5, $in4, $in3, $in2);
             storeTuitionData("out", $out14, $out13, $out12, $out11, $out10, $out9, $out8, $out7, $out6, $out5, $out4, $out3, $out2);
             calcCoeffs(); // ***MOVE THIS FUNCTION CALL OUTSIDE CONSTRUCTOR***
-            echo "Newman is a bitch";
         }
         
         //Create arrays for in and out of state in appropriate variables
@@ -45,9 +44,7 @@
         //Calculates the quadratic coefficients for in and out of state tuition and stores them in appropriate arrays
         private function calcCoeffs() {
             quadReg("in");
-            echo "Newman is bad ";
             quadReg("out");
-            echo "at cornhole";
         }
                 
         //Get quadratic formula coefficients() -> calculates values of a, b, and c for quadratic equation
@@ -82,6 +79,7 @@
                 $sumx2y += $key*$key*$currentYearCost;
             }
             
+            newvalue = ax^2 + bx + c
             // a = (( Σ x2y * Σ xx ) - (Σ xy * Σ xx2 )) / (( Σ xx * Σ x2x2) - (Σ xx2 * Σ xx2))
             // b = (( Σ xy * Σ x2x2 ) - (Σ x2y * Σ xx2 )) / (( Σ xx * Σ x2x2) - (Σ xx2 * Σ xx2 ))
             // c = ( Σ y / $numValues ) - ( b * ( Σ x / $numValues )) - ( a * ( Σ x2 / $numValues ))
